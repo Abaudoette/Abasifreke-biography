835ad913b33608d03d3cc230c76f5e861b834bba
Abasifreke <abasifrekeudoette@gmail.com>
2023-05-12 08:06:55 +0100

    added html doc css styling js commands

---
+++ script.js
@@ -0,0 +1,8 @@
+const updateMeBtn = document.getElementById("change-btn");
+    //console.log(updateMeBtn); 
+
+    updateMeBtn.addEventListener("click", function (){
+        document.getElementById("cnt").innerHTML = "I am currently learning Java Script"
+    })
+
+    
